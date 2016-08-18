1ed4497788af33afa83232c4ed83befea9f932bc
Steve Benson <steve@xelphene.net>
2016-08-18 00:56:03 -0400

    Added resolution and outputs. Project is generally working now.

--- nmine/main.py
+++ nmine/main.py
@@ -1,20 +1,22 @@
 
 import sys
+import os
 import optparse
 import logging
 
 from strfind import StringFinder
+from output import writeOutputZone
+from output import writeOutputHosts
 
-def searchPath(path):
+def extractNamesFromPath(path):
+	'''given a path to a file, extract all possible DNS names from it.'''
 	f = open(path)
-	sf = StringFinder()
+	sf=  StringFinder()
 	names = set()
 	for line in f:
 		for name in sf.searchStringConfirmed(line):
 			names.add(name)
-	
-	for name in names:
-		print ' *',name
+	return names		
 
 def parseArgs(argv=sys.argv):
 	
@@ -24,18 +26,24 @@ def parseArgs(argv=sys.argv):
 	)
 	parser.remove_option('--version')
 	parser.add_option('-i', '--interesting', dest='scope', default=None, help= '''Path to file containing IPv4 addresses or prefixes which you consider "interesting" (the scope of the search).  This program will only output DNS names resolving to these addresses.  If not specified, a file named SCOPE in the current directory will be looked for and used if found.  Either a SCOPE file or this option is required.''')
+	parser.add_option('-n', '--nameserver', dest='nameserver', default=None, help= 'Nameserver to query. The normal DNS servers for this system will be used by default.')
+	parser.add_option('-f', '--format', dest='format', default='hosts', help='Output format. Must be either "hosts" (/etc/hosts style) or "zone" (BIND zone file / dig output style).')
 	parser.add_option('-v', '--version', action='version', help='Display version number and exit')
 	parser.add_option('-d', '--debug', action='store_true', default=False, help='Turn on debug logging output')
 	
 	(opts,args) = parser.parse_args(argv)
-	
+
 	searchItems = []
-	if len(argv)>=2:
-		searchItems = argv[1:]
+	if len(args)>=2:
+		searchItems = args[1:]
 	else:
 		searchItems = ['.']
 
-	return (searchItems, opts.scope, opts.debug)
+	if opts.format not in ('zone','hosts'):
+		sys.stderr.write('Invalid value for -f/--format option. It must be "zone" or "hosts".\n')
+		raise SystemExit(1)
+
+	return (searchItems, opts)
 
 def initLogging(debug):
 	log = logging.getLogger('nmine')
@@ -88,13 +96,90 @@ def loadScope(scopePath):
 		lineNum+=1
 	
 	return scope
+
+class BadSearchPath(Exception):
+	def __init__(self, searchPath):
+		self.searchPath = searchPath
+
+class NonExistentSearchPath(BadSearchPath):
+	def __str__(self):
+		return 'search path %s does not exist' % repr(self.searchPath)
+
+class InvalidSearchPath(BadSearchPath):
+	def __str__(self):
+		return 'search path %s is neither a file nor a directory.' % repr(self.searchPath)
+
+def buildSearchFiles(searchPaths, doDotFiles=False):
+	log = logging.getLogger('nmine')
+	searchFiles = []
+	for searchPath in searchPaths:
+		if not os.path.exists(searchPath):
+			raise NonExistentSearchPath(searchPath)
+		else:
+			if os.path.isfile(searchPath):
+				log.debug('will search file %s' % searchPath)
+			elif os.path.isdir(searchPath):
+				log.debug('recursively searching directory %s for files to search' % searchPath)
+				for (dirpath, dirnames, filenames) in os.walk(searchPath, topdown=True):
+					if not doDotFiles:
+						for dirname in dirnames:
+							if dirname.startswith('.'):
+								del dirnames[dirnames.index(dirname)]
+						filenames = filter(lambda f: not f.startswith('.'), filenames)
+					for filename in filenames:		
+						sf = os.path.join(dirpath, filename)
+						log.debug('will search file %s' % sf)
+						searchFiles.append(sf)
+			else:
+				raise InvalidSearchPath(searchPath)
+	return searchFiles
+
+def resolveNames(names, scope, nameserver=None):
+	import dns.resolver
+	import iptree
+	log = logging.getLogger('nmine')
 	
+	results = {}
+	
+	if nameserver==None:
+		# use the normal system DNS servers
+		r = dns.resolver.Resolver(configure=True)
+	else:
+		# use the specified DNS server, not the system ones
+		r = dns.resolver.Resolver(configure=False)
+		r.nameservers = [nameserver]
+
+	for name in names:
+		try:
+			answer = r.query(name,'A')
+		except dns.resolver.NoNameservers:
+			pass
+		else:
+			for rdata in answer:
+				log.debug('resolved %s to %s' % (name, rdata.address))
+				prefix = iptree.Prefix(rdata.address)
+				for scopeEntry in scope:
+					log.debug('    %s in %s ?' % (prefix, scopeEntry))
+					if prefix in scopeEntry or prefix==scopeEntry:
+						log.debug('        HIT: %s' % rdata.address)
+						if results.has_key(name):
+							results[name].append(rdata.address)
+						else:
+							results[name] = [rdata.address]
+	return results
+
 def main():
 	
-	(searchItems, scopePath, debug) = parseArgs()
-	
+	(searchPaths, opts) = parseArgs()
+	scopePath = opts.scope
+	debug = opts.debug
 	initLogging(debug)
 	log = logging.getLogger('nmine')
+
+	###################################################
+	# make sure required python modules are available in a user-friendly
+	# manner
+	###################################################
 
 	try:
 		import iptree
@@ -107,6 +192,10 @@ def main():
 	except ImportError:
 		log.error("You don't seem to have dnspython installed. See http://www.dnspython.org. (On Ubuntu/Debian: apt-get install python-dnspython)")
 		raise SystemExit(1)
+
+	###################################################
+	# load the scope
+	###################################################
 
 	try:
 		scope = loadScope(scopePath)
@@ -121,6 +210,40 @@ def main():
 		for s in scope:
 			log.debug('scope item: %s' % s)
 
-	# TODO: build a list of paths to every file we will search for DNS names
-	# TODO: add options for which DNS resolver to use.
+	###################################################
+	# build up a list of paths of plain files to extract possible names from
+	###################################################
+
+	log.debug('search paths: %s' % repr(searchPaths))
+	try:
+		searchFiles = buildSearchFiles(searchPaths)
+	except BadSearchPath, bsp:
+		log.error(bsp)
+		raise SystemExit(1)
+	
+	###################################################
+	# extract possible DNS names from the files discovered above
+	###################################################
+	
+	allNames = set()
+	for searchFile in searchFiles:
+		log.debug('*** extracting names from %s' % searchFile)
+		names = extractNamesFromPath(searchFile)
+		for name in names:
+			log.debug('    %s found in %s' % (name, searchFile))
+		allNames = allNames.union(names)
+	log.debug('extraction complete. possible names found:')
+	for name in allNames:
+		log.debug('    %s' % name)
+
+	###################################################
+	# actually resolve the names
+	###################################################
+	
+	lookups = resolveNames(allNames, scope, nameserver=opts.nameserver)
+	
+	if opts.format=='zone':
+		writeOutputZone(lookups)
+	else:
+		writeOutputHosts(lookups)
 		
